ff3b5594274b07efa980054574958bfa5510655d
mysmkmq <mysmkmq@gmail.com>
2023-06-16 11:00:50 +0700

    ga jadi

--- server.js
+++ server.js
@@ -5,20 +5,9 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const router = require("./src/router/router");
 const port = process.env.PORT || 3000;
-const fs = require('fs')
-const https = require('https')
-const path = require('path')
 
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(router);
-
-const sslServer = https.createServer(
-    {
-        key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
-        cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
-    },
-    app
-)
-sslServer.listen(port);
+app.listen(port);

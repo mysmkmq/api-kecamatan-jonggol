c2a2883caa104713c1b0849ece8f72ab1c9e6654
mysmkmq <mysmkmq@gmail.com>
2023-06-19 06:24:41 +0700

    bismillah benner

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ module.exports = {
     database: process.env.DB_DEV,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
-    timezone: "+07:00",
+    timezone: "+07:00"
   },
   test: {
     username: process.env.DB_USERNAME,
@@ -13,15 +13,20 @@ module.exports = {
     database: process.env.DB_TEST,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
-    timezone: "+07:00",
+    timezone: "+07:00"
   },
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_PROD,
     host: process.env.DB_HOST,
-    port : process.env.DB_PORT,
+    port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
     timezone: "+07:00",
+    protocol: 'mysql',
+    dialectOptions: {
+      ssl: true,
+      native:true
+    }
   },
 };

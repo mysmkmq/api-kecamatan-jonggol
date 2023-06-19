98251a0dced7a632273ad8a01023e22ba064ffa6
mysmkmq <mysmkmq@gmail.com>
2023-06-19 09:04:46 +0700

    balikin lagi

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const fs = require("fs");
-const https = require("https");
-const path = require("path");
 const cookieParser = require("cookie-parser");
 const router = require("./src/router/router");
 const port = process.env.PORT || 3000;
@@ -13,9 +10,4 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(router);
-
-const sslserver = https.createServer({
-    key: fs.readFileSync(path.join(__dirname,'cert','key.pem')),
-    cert: fs.readFileSync(path.join(__dirname,'cert','cert.pem'))
-}, app);
-sslserver.listen(port);
+app.listen(port);
